58b5a58518478743433d76cdb5fa3cff13cce7dd
FangRui <fangrui_95@163.com>
2026-07-18 14:24:01 +0800

    Keep TExpands tile-native

--- lib/PTO/Transforms/PTOViewToMemref.cpp
+++ lib/PTO/Transforms/PTOViewToMemref.cpp
@@ -90,7 +90,7 @@ static bool hasMigratedTileNativeOp(func::FuncOp func) {
             pto::TDivOp, pto::TDivSOp, pto::TConcatOp,
             pto::TConcatidxOp, pto::TPartAddOp, pto::TPartMulOp,
             pto::TColExpandOp, pto::TColExpandMulOp, pto::TColExpandMaxOp,
-            pto::TColExpandMinOp>(op)) {
+            pto::TColExpandMinOp, pto::TExpandsOp>(op)) {
       found = true;
       return WalkResult::interrupt();
     }
