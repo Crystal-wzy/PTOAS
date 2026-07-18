f7555b54fe0f33fc6970cd39cff6f732255232be
FangRui <fangrui_95@163.com>
2026-07-18 15:22:58 +0800

    Keep TCvt tile-native

--- lib/PTO/Transforms/PTOViewToMemref.cpp
+++ lib/PTO/Transforms/PTOViewToMemref.cpp
@@ -95,7 +95,8 @@ static bool hasMigratedTileNativeOp(func::FuncOp func) {
             pto::TExtractFPOp, pto::TInsertFPOp, pto::TAddCOp,
             pto::TAddSCOp, pto::TMovFPOp, pto::TFillPadOp,
             pto::TFillPadInplaceOp, pto::TLoadOp, pto::TStoreOp,
-            pto::TColMaxOp, pto::TColMinOp, pto::TColSumOp>(op)) {
+            pto::TColMaxOp, pto::TColMinOp, pto::TColSumOp,
+            pto::TCvtOp>(op)) {
       found = true;
       return WalkResult::interrupt();
     }
