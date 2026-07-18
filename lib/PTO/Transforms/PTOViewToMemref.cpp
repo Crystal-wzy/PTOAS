e1ac0eaee3411b969692f06094019312ea4e366c
FangRui <fangrui_95@163.com>
2026-07-18 16:36:35 +0800

    Keep TMrgSort tile-native

--- lib/PTO/Transforms/PTOViewToMemref.cpp
+++ lib/PTO/Transforms/PTOViewToMemref.cpp
@@ -97,7 +97,8 @@ static bool hasMigratedTileNativeOp(func::FuncOp func) {
             pto::TFillPadInplaceOp, pto::TLoadOp, pto::TStoreOp,
             pto::TColMaxOp, pto::TColMinOp, pto::TColSumOp,
             pto::TCvtOp, pto::TGatherOp, pto::TGatherBOp, pto::TCIOp,
-            pto::TPrintOp, pto::TQuantOp, pto::TTransOp>(op)) {
+            pto::TPrintOp, pto::TQuantOp, pto::TTransOp,
+            pto::TMrgSortOp>(op)) {
       found = true;
       return WalkResult::interrupt();
     }
