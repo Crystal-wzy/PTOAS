b267f734aa22b1379995614634977a1dfcd91137
FangRui <fangrui_95@163.com>
2026-07-19 21:04:50 +0800

    Keep TSort32 tile-native

--- lib/PTO/Transforms/PTOViewToMemref.cpp
+++ lib/PTO/Transforms/PTOViewToMemref.cpp
@@ -115,7 +115,8 @@ static bool hasMigratedTileNativeOp(func::FuncOp func) {
             pto::TPartArgMaxOp, pto::TPartArgMinOp, pto::TPowOp,
             pto::TPowSOp, pto::TRsqrtOp, pto::TPReluOp,
             pto::TRemOp, pto::TRemSOp, pto::TSelOp,
-            pto::TSelSOp, pto::TXorOp, pto::TXorSOp>(op)) {
+            pto::TSelSOp, pto::TXorOp, pto::TXorSOp,
+            pto::TSort32Op>(op)) {
       found = true;
       return WalkResult::interrupt();
     }
